685b39b20fd4e874c613abb521b3869b2be533d5
luigiazoreng <luigiazoreng@gmail.com>
2023-11-29 17:14:35 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	handlers "working/Handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":3000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+
+	}()
+	sigChan := make(chan os.Signal, 10)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	d := 30 * time.Second
+	tc, cancel := context.WithTimeout(context.Background(), d)
+	s.Shutdown(tc)
+	defer cancel()
+}
